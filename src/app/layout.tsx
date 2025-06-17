5e8dde417b321c8ab9d315ad6378b1275383b795
Takayuki Kitamoto <t.kitamoto@mailfence.com>
2025-06-17 10:05:39 +0900

    11: agents form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 
 import { TRPCReactProvider } from '@/trpc/client';
 import './globals.css';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({
   variable: '--font-geist-sans',
@@ -22,7 +23,10 @@ export default function RootLayout({
   return (
     <TRPCReactProvider>
       <html lang="en">
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <body className={`${inter.className} antialiased`}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </TRPCReactProvider>
   );
