9b7a41ee9a40f5aa77059e46123af2f3ad73b978
Takayuki Kitamoto <t.kitamoto@mailfence.com>
2025-06-23 09:50:44 +0900

    19: meetings filters

--- src/modules/meetings/ui/components/meetings-list-header.tsx
+++ src/modules/meetings/ui/components/meetings-list-header.tsx
@@ -1,12 +1,31 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { PlusIcon } from 'lucide-react';
+import { PlusIcon, XCircleIcon } from 'lucide-react';
 import { NewMeetingDialog } from './new-meeting-dialog';
 import { useState } from 'react';
+import { MeetingsSearchFilter } from './meetings-search-filter';
+import { StatusFilter } from './status-filter';
+import { AgentIdFilter } from './agent-id-filter';
+import { useMeetingsFilters } from '../../hooks/use-meetings-filters';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 
 export const MeetingsListHeader = () => {
+  const [filters, setFilters] = useMeetingsFilters();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const isAnyFilterModified =
+    !!filters.status || !!filters.search || !!filters.agentId;
+
+  const onClearFIlters = () => {
+    setFilters({
+      status: null,
+      agentId: '',
+      search: '',
+      page: 1,
+    });
+  };
+
   return (
     <>
       <NewMeetingDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
@@ -18,7 +37,20 @@ export const MeetingsListHeader = () => {
             New Meeting
           </Button>
         </div>
-        <div className="flex items-center gap-x-2 p-1">TODO: Filters</div>
+        <ScrollArea>
+          <div className="flex items-center gap-x-2 p-1">
+            <MeetingsSearchFilter />
+            <StatusFilter />
+            <AgentIdFilter />
+            {isAnyFilterModified && (
+              <Button variant="outline" onClick={onClearFIlters}>
+                <XCircleIcon className="size-4" />
+                Clear
+              </Button>
+            )}
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </div>
     </>
   );
